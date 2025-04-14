98ef042880b8f9c8d2473d755e1d4423145a67bd
KanadTelang <kanadpanini.telang@stonybrook.edu>
2025-04-14 18:43:17 -0400

    TreeNode

--- src/TreeNode.h
+++ src/TreeNode.h
@@ -1,35 +1,35 @@
-#ifndef TreeStruct_h
-#define TreeStruct_h
+#ifndef TreeNode_h
+#define TreeNode_h
 
 using namespace std; 
 
-class TreeStruct
+class TreeNode
 {
 private:
     /* data */
-    TreeStruct* rightChild_; 
-    TreeStruct* leftChild_; 
+    TreeNode* rightChild_; 
+    TreeNode* leftChild_; 
     int width_; 
     int height_; 
     int NodeIndex; // This is the index of the node in the vector nodes. Everything else is gonna be -1 unless  we get to the leaf node. 
 
 public:
-    TreeStruct(){
+    TreeNode(){
         rightChild_ = nullptr; 
         leftChild_ = nullptr; 
         NodeIndex = -1; 
         width_ = -1; 
         height_ = -1; 
     };
-    ~TreeStruct();
+    ~TreeNode();
 
     int getWidth() {return width_;}
     int getHeight() {return height_;}
-    TreeStruct* getRightChild() {return rightChild_;}
-    TreeStruct* getLeftChild() {return leftChild_;}
+    TreeNode* getRightChild() {return rightChild_;}
+    TreeNode* getLeftChild() {return leftChild_;}
 
-    void setRightChild(TreeStruct* right)  {rightChild_ = right; }
-    void setLeftChild(TreeStruct* left)  {leftChild_ = left;}    
+    void setRightChild(TreeNode* right)  {rightChild_ = right; }
+    void setLeftChild(TreeNode* left)  {leftChild_ = left;}    
     void setNodeIndex(int idx) {NodeIndex = idx;}
     void setWidth(int width){width_ = width;}
     void setHeight(int height){height_ = height;}
